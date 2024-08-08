de6ab3b9b37d2192d9fb5ecb014685aa24c1c9df
Neil Enns <neile@live.com>
2024-08-08 13:41:08 -0700

    Initial version

---
+++ README.md
@@ -0,0 +1,21 @@
+# Stream Deck transport for Winston
+
+This transport logs messages using the Stream Deck SDK's logger.
+
+## Sample usage
+
+```typescript
+import winston from 'winston';
+import StreamdeckTransport from 'streamdeck-transport';
+
+const Logger = winston.createLogger({
+  level: 'info',
+  transports: [new StreamdeckTransport({ scope: 'my-plugin-name' })],
+});
+```
+
+The maximum log level to log can be configured using the `level` property in the constructor options:
+
+```typescript
+    new StreamdeckTransport({ scope: "my-plugin-name", level: "info" }),
+```
---
+++ src/index.ts
@@ -0,0 +1,81 @@
+import Transport from 'winston-transport';
+import streamDeck, { LogLevel } from '@elgato/streamdeck';
+import { LEVEL } from 'triple-beam';
+import { TransformableInfo } from 'logform';
+
+/**
+ * Options for the Streamdeck transport.
+ */
+type StreamdeckTransportOptions = Transport.TransportStreamOptions & {
+  /**
+   * Sets the logging scope for the Streamdeck logger.
+   */
+  scope?: string;
+};
+
+/**
+ * Converts a Winston log level to a Streamdeck LogLevel.
+ * @param level The Winston log level
+ * @returns The Streamdeck LogLevel, or LogLevel.INFO if the Winston log level couldn't be mapped
+ */
+function stringToLogLevel(level: string): LogLevel {
+  // Convert the string to uppercase and check if it matches an enum key
+  const upperCaseLevel = level.toUpperCase();
+  // Use bracket notation to access enum value
+  if (upperCaseLevel in LogLevel) {
+    return LogLevel[upperCaseLevel as keyof typeof LogLevel];
+  } else {
+    return LogLevel.INFO; // Default to INFO
+  }
+}
+
+/**
+ * Logs Winston messages using the Streamdeck logger.
+ */
+class StreamdeckTransport extends Transport {
+  private logger: ReturnType<typeof streamDeck.logger.createScope>;
+
+  /**
+   * Creates a new Streamdeck transport with the specified scope and Winston log level.
+   * @param opts The options for the transport
+   */
+  constructor(opts?: StreamdeckTransportOptions) {
+    super(opts);
+
+    this.logger = streamDeck.logger.createScope(opts?.scope ?? '');
+    this.logger.setLevel(stringToLogLevel(opts?.level ?? 'INFO'));
+  }
+
+  /**
+   * Logs the info to the Streamdeck logger.
+   * @param info The info to log
+   * @param callback The next function to call
+   */
+  log(info: TransformableInfo, callback: () => void) {
+    const json = JSON.stringify(info);
+    const winstonLevel = info[LEVEL];
+
+    switch (winstonLevel) {
+      case 'debug':
+      case 'trace':
+        this.logger.debug(json);
+        break;
+      case 'error':
+        this.logger.error(json);
+        break;
+      case 'info':
+        this.logger.info(json);
+        break;
+      case 'warn':
+        this.logger.warn(json);
+        break;
+      default:
+        this.logger.info(json);
+        break;
+    }
+
+    callback();
+  }
+}
+
+export default StreamdeckTransport;
